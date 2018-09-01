6dd1ee3724f90d2cf49d213610a1d7e0a9f64a25
Apple <apple@Apples-iMac.local>
2018-09-01 22:04:09 +0530

    Intial Commit

---
+++ Mentions/MentionsManager.swift
@@ -0,0 +1,13 @@
+//
+//  MentionsManager.swift
+//  Mentions
+//
+//  Created by Apple on 01/09/18.
+//  Copyright © 2018 Batth. All rights reserved.
+//
+
+import UIKit
+
+class MentionsManager: NSObject {
+
+}
---
+++ Mentions/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Mentions
+//
+//  Created by Apple on 01/09/18.
+//  Copyright © 2018 Batth. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
